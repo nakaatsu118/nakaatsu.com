4bcf5ceb653f8c1677425d28c6c8d50c70def417
Atsushi Nakashima <nakaatsumain@gmail.com>
2022-03-23 02:58:09 +0900

    build error 修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,12 +72,12 @@ const Home: NextPage = () => {
           <div className='mockup-window bg-slate-400 md:w-9/12 w-10/12 shadow-2xl max-w-5xl'>
             <div className='flex flex-col justify-center items-center gap-4 bg-slate-500 bg-opacity-90 py-10'>
               <div className='relative md:w-60 md:h-60 w-40 h-40'>
-                <Image className='mask mask-squircle' src="/images/nakaatsu_icon.png" layout='fill' objectFit={'contain'} />
+                <img className='mask mask-squircle' src="/images/nakaatsu_icon.png"  />
               </div>
               <div className='md:block hidden mockup-code bg-opacity-30 border-base-content border-2 border-opacity-10 w-4/6'>
-                <pre data-prefix="$"><code>Welcome to nakaatsu's site.</code></pre>
+                <pre data-prefix="$"><code>Welcome to nakaatsu&apos;s site.</code></pre>
               </div>
-              <div className='md:hidden text-white font-mono'>$ Welcome to<br />nakaatsu's site.</div>
+              <div className='md:hidden text-white font-mono'>$ Welcome to<br />nakaatsu&apos;s site.</div>
             </div>
           </div>
         </div>
