7fcba4d3da13c0eb1e5d6b3fde0a0a151cdd1551
Atsushi Nakashima <nakaatsumain@gmail.com>
2022-07-10 01:26:00 +0900

    フッター追加

---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const Footer: NextPage = () => {
+  return (
+    <div id="Footer" className="bg-slate-800 py-8 px-6 flex justify-center">
+      <div className="font-raleway text-sm">
+        <a className="link text-white" href="" target="_blank" rel='noopener noreferrer'>@nakaatsu</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
