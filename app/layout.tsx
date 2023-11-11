0fde3b15763d5f33259d4472904c8f4d09fdacc3
nakaatsu118 <nakaatsumain@gmail.com>
2023-11-12 01:40:16 +0900

    不要package削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import "@/_styles/normalize.css"
 import "@/_styles/globals.css"
 import styles from '@/Root.module.css'
 import Navigation from "@/_components/Navigation"
+import { Metadata } from "next"
 
-export const siteTitle = 'nakaatsu World'
+const siteTitle = 'nakaatsu World'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: siteTitle,
   description: 'nakaatsuのポートフォリオサイトです。自身の経歴やスキルセット等をまとめています。',
 }
