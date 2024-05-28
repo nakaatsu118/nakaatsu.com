3ec92c559dc1d897126d1877f5a13290e2394163
nakaatsu <nakashima@microcms.co.jp>
2024-05-28 16:46:43 +0900

    feat: Update lazy loading strategy for iframe.ly script

--- app/blog/[blogId]/blogId.component.tsx
+++ app/blog/[blogId]/blogId.component.tsx
@@ -1,87 +1,54 @@
-'use client';
-
 import Card from '~/_components/Card';
 import CardHeader from '~/_components/Card/CardHeader';
 import Footer from '~/_components/Footer';
 import MotionWrapper from '~/_components/MotionWrapper';
 import ProgressBar from '~/_components/ProgressBar';
 import { formatDate } from '~/_libs/formatDate';
-import { BlogIdProps } from './page';
-import { Category, getBlogDetail } from '~/_libs/microcms';
+import { Blog } from '~/_libs/microcms';
 import styles from './BlogPage.module.css';
-import { useEffect, useState } from 'react';
-import {
-  MicroCMSContentId,
-  MicroCMSDate,
-  MicroCMSImage,
-} from 'microcms-js-sdk';
-import Loading from '~/_components/Loading';
 import Script from 'next/script';
+import parse, { HTMLReactParserOptions } from 'html-react-parser';
 
-const fetchBlogDetail = async (blogId: string) => {
-  const res = await getBlogDetail(blogId);
-  return res;
+const options: HTMLReactParserOptions = {
+  replace: ({ attribs, name }: any) => {
+    if (!attribs || Object.keys(attribs).length === 0) return;
+
+    // ハイドレーションエラーが生じるため、悪さをしているscriptタグを削除
+    if (name === 'script' && attribs.src === '//cdn.iframe.ly/embed.js') {
+      return <></>;
+    }
+  },
 };
 
-export const BlogIdComponent = async ({ params }: BlogIdProps) => {
-  const { blogId } = params;
-  const [blogData, setBlogData] = useState<
-    | ({
-        id: string;
-        title: string;
-        content: string;
-        eyecatch?: MicroCMSImage | undefined;
-        category?: Category | undefined;
-      } & MicroCMSDate &
-        MicroCMSContentId)
-    | null
-  >(null);
-
-  useEffect(() => {
-    fetchBlogDetail(blogId).then((res) => {
-      setBlogData(res);
-    });
-    const script = document.createElement('script');
-    script.src = '//cdn.iframe.ly/embed.js';
-    script.async = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  if (!blogData) {
-    return <Loading />;
-  }
-
+export const BlogIdComponent = async ({
+  content,
+  title,
+  publishedAt,
+  category,
+  eyecatch,
+}: Blog) => {
   return (
     <>
-      <Script src="//cdn.iframe.ly/embed.js" strategy="lazyOnload" />
       <MotionWrapper>
         <ProgressBar />
         <Card>
           <CardHeader
             iconPath="/images/notebook.svg"
             iconAlt="blog"
-            title={blogData.title}
+            title={title}
             link={''}
             isShare
-            shareTitle={blogData.title}
+            shareTitle={title}
           />
           <div className={styles.blogPageContainer}>
             <div className={styles.infoContainer}>
-              <time>
-                {blogData.publishedAt ? formatDate(blogData.publishedAt) : ''}
-              </time>
-              <div className={styles.category}>{blogData.category?.name}</div>
+              <time>{publishedAt ? formatDate(publishedAt) : ''}</time>
+              <div className={styles.category}>{category?.name}</div>
             </div>
             <div className={styles.imageContainer}>
-              <img src={blogData.eyecatch?.url + '?w=960'} />
+              <img src={eyecatch?.url + '?w=960'} />
             </div>
-            <div
-              className={styles.content}
-              dangerouslySetInnerHTML={{ __html: `${blogData.content}` }}
-            />
+            <div className={styles.content}>{parse(content, options)}</div>
           </div>
         </Card>
         <Footer />
