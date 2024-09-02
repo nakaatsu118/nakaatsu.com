6b427f298ce08d26784d5d72272b3bd0b8d252ac
nakaatsu118 <nakaatsumain@gmail.com>
2024-09-03 03:23:04 +0900

    feat: Add lazy loading for images and update styles
    chore: Update npm dependencies
    
    fix: Adjust margin-bottom and line-height in BlogPage module CSS
    
    feat: Add privacy policy link in footer
    
    chore: Add privacy policy page
    
    fix: Add CSS for table in blog design
    
    fix: Adjust line-height in blog design
    
    fix: Update blog article design

--- app/_components/TopPage/WhatIs/index.tsx
+++ app/_components/TopPage/WhatIs/index.tsx
@@ -1,9 +1,13 @@
-'use client'
+'use client';
 
 import Image from 'next/image';
 import styles from './WhatIs.module.css';
 import CardHeader from '../../Card/CardHeader';
-import { GithubOutlined, InstagramOutlined, TwitterOutlined } from '@ant-design/icons';
+import {
+  GithubOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+} from '@ant-design/icons';
 import Card from '~/_components/Card';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
@@ -12,27 +16,92 @@ const WhatIs = () => {
   return (
     <div className={styles.whatIsWrapper}>
       <Card>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2, delay: 0.3 }}>
-          <CardHeader iconPath='/images/Icon_nakaatsu_.png' iconAlt='nakaatsu icon' title='Welcome!' link='/' isShare />
-          <motion.div className={`${styles.imageContainer}`} initial={{ x: 400, y: 200, scale: 0 }} animate={{ x: 0, y: 0, scale: 1 }} transition={{ type: 'spring', duration: 0.48, delay: 0.6, bounce: 0.3 }}>
-            <Image src='https://images.microcms-assets.io/assets/af60901723f5492a9a451bcfbe5ace1c/f5eb79b485a54f5b9a550bbf0a440121/nakaatsu_top.png?fit=crop&w=1200' alt="main Image" fill style={{ objectFit: 'cover' }} priority />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2, delay: 0.3 }}
+        >
+          <CardHeader
+            iconPath="/images/Icon_nakaatsu_.webp"
+            iconAlt="nakaatsu icon"
+            title="Welcome!"
+            link="/"
+            isShare
+          />
+          <motion.div
+            className={`${styles.imageContainer}`}
+            initial={{ x: 400, y: 200, scale: 0 }}
+            animate={{ x: 0, y: 0, scale: 1 }}
+            transition={{
+              type: 'spring',
+              duration: 0.48,
+              delay: 0.6,
+              bounce: 0.3,
+            }}
+          >
+            <Image
+              src="/images/nakaatsu_top.webp"
+              alt="main Image"
+              fill
+              style={{ objectFit: 'cover' }}
+              priority
+            />
           </motion.div>
           <div className={styles.mainContent}>
             <div className={styles.actionContainer}>
               <div className={styles.leftAction}>
-                <Link href={'https://twitter.com/nakaatsu'} rel="noopener noreferrer" target="_blank">
-                  <motion.div whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.3 } }} whileTap={{ scale: 0.8 }}>
-                    <TwitterOutlined className={`${styles.actionIcon} ${styles.twitter}`} />
+                <Link
+                  href={'https://twitter.com/nakaatsu'}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <motion.div
+                    whileHover={{
+                      scale: 1.2,
+                      rotate: 360,
+                      transition: { duration: 0.3 },
+                    }}
+                    whileTap={{ scale: 0.8 }}
+                  >
+                    <TwitterOutlined
+                      className={`${styles.actionIcon} ${styles.twitter}`}
+                    />
                   </motion.div>
                 </Link>
-                <Link href={'https://www.instagram.com/nakaatsu_world/'} rel="noopener noreferrer" target="_blank">
-                  <motion.div whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.3 } }} whileTap={{ scale: 0.8 }}>
-                    <InstagramOutlined className={`${styles.actionIcon} ${styles.instagram}`} />
+                <Link
+                  href={'https://www.instagram.com/nakaatsu_world/'}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <motion.div
+                    whileHover={{
+                      scale: 1.2,
+                      rotate: 360,
+                      transition: { duration: 0.3 },
+                    }}
+                    whileTap={{ scale: 0.8 }}
+                  >
+                    <InstagramOutlined
+                      className={`${styles.actionIcon} ${styles.instagram}`}
+                    />
                   </motion.div>
                 </Link>
-                <Link href={'https://github.com/nakaatsu118'} rel="noopener noreferrer" target="_blank">
-                  <motion.div whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.3 } }} whileTap={{ scale: 0.8 }}>
-                    <GithubOutlined className={`${styles.actionIcon} ${styles.github}`} />
+                <Link
+                  href={'https://github.com/nakaatsu118'}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <motion.div
+                    whileHover={{
+                      scale: 1.2,
+                      rotate: 360,
+                      transition: { duration: 0.3 },
+                    }}
+                    whileTap={{ scale: 0.8 }}
+                  >
+                    <GithubOutlined
+                      className={`${styles.actionIcon} ${styles.github}`}
+                    />
                   </motion.div>
                 </Link>
               </div>
@@ -41,19 +110,25 @@ const WhatIs = () => {
               <h3 className={styles.title}>このサイトについて</h3>
             </div>
             <div className={styles.bodyContainer}>
-              こんにちは。なかあつです。このサイトは <Link href='/profile' className={styles.link}>@nakaatsu</Link> の日々をしたためています。<br />
-              多趣味でごっちゃにななかあつの世界がここにあります。<br />
+              こんにちは。なかあつです。このサイトは
+              <Link href="/profile" className={styles.link}>
+                @nakaatsu
+              </Link>
+              の日々をしたためています。
               <br />
-              🔥Next.js / React / TypeScript<br />
+              多趣味でごっちゃにななかあつの世界がここにあります。
+              <br />
+              <br />
+              🔥Next.js / React / TypeScript
+              <br />
               🐠Aquarium / 🌿Plants / 👨‍🍳Cooking / 📸Gadget
               <br />
             </div>
           </div>
-        </motion.div >
-      </Card >
+        </motion.div>
+      </Card>
     </div>
+  );
+};
 
-  )
-}
-
-export default WhatIs
+export default WhatIs;
--- app/profile/Biography/index.tsx
+++ app/profile/Biography/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import CardHeader from '~/_components/Card/CardHeader';
 import styles from './Biography.module.css';
@@ -10,27 +10,61 @@ const Biography = () => {
     <div className={styles.biographyWrapper}>
       <Card>
         <motion.div>
-          <CardHeader iconPath='/images/biography.png' iconAlt='biography' title='Biography' />
+          <CardHeader
+            iconPath="/images/biography.webp"
+            iconAlt="biography"
+            title="Biography"
+          />
           <div className={styles.biographyContainer}>
-            <motion.div className={styles.historyContainer} initial={{ x: '100%', opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 0.4, delay: 0.1, type: 'tween' }} viewport={{ margin: '80px' }}>
+            <motion.div
+              className={styles.historyContainer}
+              initial={{ x: '100%', opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.1, type: 'tween' }}
+              viewport={{ margin: '80px' }}
+            >
               <div className={styles.history}>
                 <p className={styles.date}>2016/4</p>
-                <p className={styles.description}>SIer企業に入社。インターネット決済システムの設計、試験を担当。</p>
+                <p className={styles.description}>
+                  SIer企業に入社。インターネット決済システムの設計、試験を担当。
+                </p>
               </div>
             </motion.div>
-            <motion.div className={styles.historyContainer} initial={{ x: '-100%', opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.1, type: 'tween' }} viewport={{ margin: '80px' }}>
+            <motion.div
+              className={styles.historyContainer}
+              initial={{ x: '-100%', opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.3, delay: 0.1, type: 'tween' }}
+              viewport={{ margin: '80px' }}
+            >
               <div className={styles.history}>
                 <p className={styles.date}>2018/9</p>
-                <p className={styles.description}>いちから株式会社（現ANYCOLOR株式会社）に入社。WEBフロント開発、サーバーサイド、社内システム、LP制作・監修、PMなどを担当。</p>
+                <p className={styles.description}>
+                  いちから株式会社（現ANYCOLOR株式会社）に入社。WEBフロント開発、サーバーサイド、社内システム、LP制作・監修、PMなどを担当。
+                </p>
               </div>
             </motion.div>
-            <motion.div className={styles.historyContainer} initial={{ x: '100%', opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 0.2, delay: 0.1, type: 'tween' }} viewport={{ margin: '80px' }}>
+            <motion.div
+              className={styles.historyContainer}
+              initial={{ x: '100%', opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.2, delay: 0.1, type: 'tween' }}
+              viewport={{ margin: '80px' }}
+            >
               <div className={styles.history}>
                 <p className={styles.date}>2022/2</p>
-                <p className={styles.description}>freee株式会社に入社。申告システムの機能追加や改修をフルスタックで担当。</p>
+                <p className={styles.description}>
+                  freee株式会社に入社。申告システムの機能追加や改修をフルスタックで担当。
+                </p>
               </div>
             </motion.div>
-            <motion.div className={styles.historyContainer} initial={{ x: '-100%', opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.1, type: 'tween' }} viewport={{ margin: '80px' }}>
+            <motion.div
+              className={styles.historyContainer}
+              initial={{ x: '-100%', opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.1, delay: 0.1, type: 'tween' }}
+              viewport={{ margin: '80px' }}
+            >
               <div className={styles.history}>
                 <p className={styles.date}>2022/11</p>
                 <p className={styles.description}>株式会社microCMSに入社。</p>
@@ -40,7 +74,7 @@ const Biography = () => {
         </motion.div>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Biography
+export default Biography;
